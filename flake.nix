b7adbc0fd2b209cd2f091e036b9b81743352c4b6
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-08-15 20:52:19 +0200

    google-cloud-sdk: add package
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -23,6 +23,7 @@
         {
           default = pkgs.mkShell {
             packages = with pkgs; [
+              google-cloud-sdk # https://search.nixos.org/packages?channel=unstable&type=packages&show=google-cloud-sdk
               nodejs-slim # https://search.nixos.org/packages?channel=unstable&type=packages&show=nodejs-slim
             ];
 
@@ -38,7 +39,6 @@
               export PATH="$VERCEL_PREFIX/bin:$PATH"
 
               echo "Vercel CLI is ready to use!"
-              vercel --version
             '';
           };
         }
